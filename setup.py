64ee4039d307bda6d9f273879a0f801db2f85ec2
Mikhail Pak <mikhail.pak@tum.de>
2019-12-15 23:14:58 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="mytbx",
+    version="0.1",
+    author="Mikhail Pak",
+    author_email="code@mp4096.com",
+    url="https://github.com/mp4096/mytbx",
+    packages=["mytbx"],
+    license="MIT",
+    )
